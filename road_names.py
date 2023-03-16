7bd7a6fda356d8e3f17f8df87104fb966270d301
Fraser Parlane <mail@fraserparlane.com>
2023-03-16 12:18:04 -0700

    OSM file reading refactored. Passing.

--- road_names.py
+++ road_names.py
@@ -1,40 +1,61 @@
+from dataclasses import dataclass
+from typing import Optional
+from lxml import etree
 import urllib.request
 from tqdm import tqdm
+import numpy as np
 import logging
 import os
 
 
+@dataclass
+class BBox:
+    lon_min: float
+    lon_max: float
+    lat_min: float
+    lat_max: float
+
+    def __post_init__(self):
+        self.lon_min_str = f'{self.lon_min:.4f}'
+        self.lon_max_str = f'{self.lon_max:.4f}'
+        self.lat_min_str = f'{self.lat_min:.4f}'
+        self.lat_max_str = f'{self.lat_max:.4f}'
+        self.id = f'{self.lon_min_str}_{self.lon_max_str}_{self.lat_min_str}_{self.lat_max_str}'
+
+        # Calculate map scaling values
+        self.lon_mid = (self.lon_min + self.lon_max) / 2
+        self.lat_mid = (self.lat_min + self.lat_max) / 2
+        self.lon_span = self.lon_max - self.lon_min
+        self.lat_span = self.lat_max - self.lat_min
+        self.lon_scale = np.cos(self.lat_mid * np.pi / 180)
+        self.htw_ratio = self.lat_span / (self.lon_span * self.lon_scale)
+
+
 def _get_osm_file_cached(
-        min_lon: float,
-        max_lon: float,
-        min_lat: float,
-        max_lat: float,
+        bbox: BBox,
+        use_cache: bool = True,
 ) -> bytes:
     """
     Get an OSM file from OpenStreetMaps for the given bounding box. If already
     downloaded, return cached file.
     """
 
-    # Convert the floats to standardized strings
-    min_lon_str = f'{min_lon:.4f}'
-    max_lon_str = f'{max_lon:.4f}'
-    min_lat_str = f'{min_lat:.4f}'
-    max_lat_str = f'{max_lat:.4f}'
-
     # Check if file already exists
-    filename = f'{min_lon_str}_{max_lon_str}_{min_lat_str}_{max_lat_str}.osm'
+    filename = f'{bbox.id}.osm'
     filepath = f'osm/{filename}'
-    if os.path.isfile(filepath):
-        logging.debug(f'Cached file exists: {filepath}')
+    cache_available = True
+    if use_cache:
+        logging.debug('Cached being used.')
+        if os.path.isfile(filepath):
+            logging.debug(f'Cached file exists: {filepath}')
+        else:
+            logging.debug(f'Cached file {filepath} does not exist.')
+            cache_available = False
     else:
-        logging.debug(f'Cached file {filepath} does not exist.')
-        _get_osm_file(
-            min_lon_str=min_lon_str,
-            max_lon_str=max_lon_str,
-            min_lat_str=min_lat_str,
-            max_lat_str=max_lat_str,
-            filepath=filepath,
-        )
+        cache_available = False
+
+    if not cache_available:
+        _get_osm_file(bbox=bbox)
 
     # Load and return
     with open(filepath, 'rb') as f:
@@ -43,11 +64,7 @@ def _get_osm_file_cached(
 
 
 def _get_osm_file(
-        min_lon_str: str,
-        max_lon_str: str,
-        min_lat_str: str,
-        max_lat_str: str,
-        filepath: str,
+        bbox: BBox,
 ) -> None:
     """
     Download an OSM file from OpenStreetMaps for the given bounding box.
@@ -58,11 +75,59 @@ def _get_osm_file(
         os.makedirs('osm')
 
     # Download data
-    url_suffix = f'{min_lon_str},{min_lat_str},{max_lon_str},{max_lat_str}'
+    url_suffix = f'{bbox.lon_min_str},{bbox.lat_min_str},{bbox.lon_max_str},{bbox.lat_max_str}'
     url = f'https://api.openstreetmap.org/api/0.6/map?bbox={url_suffix}'
+    filepath = f'osm/{bbox.id}.osm'
     logging.debug(f'Downloading {url}')
     urllib.request.urlretrieve(url, filepath)
 
 
+class RoadNames:
+    def __init__(
+            self,
+            use_cache: bool = True
+    ):
+        self.bbox: Optional[BBox] = None
+        self.use_cache = use_cache
+        self.osm: Optional[bytes] = None
+        self.xml: Optional[lxml.etree._Element] = None
+
+    def load_box(
+            self,
+            lon_min: float,
+            lon_max: float,
+            lat_min: float,
+            lat_max: float,
+    ) -> None:
+        """
+        Define the plotting area with a bounding box of lon, lat.
+        """
+        self.bbox = BBox(
+            lon_min=lon_min,
+            lon_max=lon_max,
+            lat_min=lat_min,
+            lat_max=lat_max,
+        )
+
+    def _read_osm(self):
+        """
+        Load the OSM file into memory.
+        """
+        # Load OSM
+        self.osm = _get_osm_file_cached(bbox=self.bbox)
+
+        # Convert to XML
+        logging.debug('Converting OSM to XML.')
+        self.xml = etree.fromstring(self.osm)
+
+    def plot(
+            self,
+            filename: str = 'default',
+    ):
+
+        # Load the data
+        self._read_osm()
+
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    pass
--- road_names_test.py
+++ road_names_test.py
@@ -1,4 +1,4 @@
-from road_names import RoadNames, _get_osm_file_cached
+from road_names import RoadNames, BBox, _get_osm_file_cached
 import unittest
 import logging
 import shutil
@@ -6,6 +6,14 @@ import os
 
 
 logging.basicConfig(level=logging.DEBUG)
+
+# Define a small area to use for testing
+small_area = {
+    'lon_min': -123.1565,
+    'lon_max': -123.1381,
+    'lat_min': 49.2721,
+    'lat_max': 49.281,
+}
 
 
 class TestRoadNames(unittest.TestCase):
@@ -15,18 +23,18 @@ class TestRoadNames(unittest.TestCase):
         if os.path.exists('osm'):
             shutil.rmtree('osm')
 
+        # Make BBox
+        bbox = BBox(**small_area)
+
         # Test uncached
-        _get_osm_file_cached(
-            min_lon=-123.1565,
-            max_lon=-123.1381,
-            min_lat=49.2721,
-            max_lat=49.281,
-        )
+        _get_osm_file_cached(bbox=bbox)
 
         # Test cached
-        _get_osm_file_cached(
-            min_lon=-123.1565,
-            max_lon=-123.1381,
-            min_lat=49.2721,
-            max_lat=49.281,
-        )
+        _get_osm_file_cached(bbox=bbox)
+
+    def test_load_box(self):
+
+        rn = RoadNames()
+        rn.load_box(**small_area)
+        rn.plot()
+
